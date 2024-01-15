6b9c8d3ad88b5622db38c965988f09fe72c98074
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-01-15 14:40:47 -0300

    .

---
+++ CalculadoraNode/cal.js
@@ -0,0 +1,12 @@
+var somaFuc = require("./soma"); //chamando o modulo.
+var subFuc = require("./sub") //chamando o modulo.
+var multiFuc = require("./multi") //chamando o modulo.
+
+console.log(somaFuc(1,2));
+console.log(subFuc(1,2));
+console.log(multiFuc(1,2));
+
+
+
+
+
