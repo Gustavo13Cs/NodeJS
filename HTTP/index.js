66d416ed2be89c8ba6d02215e427b13e61917cf5
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-01-17 00:37:49 -0300

    .

--- HTTP/index.js
+++ HTTP/index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Seja Bem-Vindo ao meu app!")
+    res.sendFile(__dirname + "/HTML/index.html") // __dirname e pra pegar o diretoria raiz
 })
 
 app.get("/sobre", function(req,res) {
-    res.send("Minha pagina sobre");
+    res.sendFile(__dirname + "/HTML/sobre.html");
 })
 
 app.get("/blog", function(req,res) {
     res.send("Bem vindo ao meu blog")
 })
+
+app.get('/ola/:cargo/:nome/:cor', function(req,res) {
+    res.send("<h1> Olá " + req.params.nome + "</h1>" +
+    "<h2> Seu Cargo e:  " + req.params.cargo + "</h2>" + 
+    "<h3> Sua cor favorita e:  " + req.params.cor + "</h3>")
+}) // passando parametros pela url
+// o send so pode usar uma vez
 
 
 app.listen(8081, function() {
